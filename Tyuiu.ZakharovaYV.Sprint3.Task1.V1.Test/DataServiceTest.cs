c351537bea76000270c85612e493b7fea4695ac4
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-10-22 15:40:33 +0500

    Task1.V1

---
+++ Tyuiu.ZakharovaYV.Sprint3.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZakharovaYV.Sprint3.Task1.V1.Lib;
+
+namespace Tyuiu.ZakharovaYV.Sprint3.Task1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMuliplySeries()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 7;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            double wait = 5.31084;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZakharovaYV.Sprint3.Task1.V1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZakharovaYV.Sprint3.Task1.V1.Lib;
+
+namespace Tyuiu.ZakharovaYV.Sprint3.Task1.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int startValue = 1;
+            int stopValue = 7;
+
+            Console.WriteLine("Старт Шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Произведение ряда =" + ds.GetMultiplySeries(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
